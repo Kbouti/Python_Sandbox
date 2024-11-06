a14515bc019d6edd0452f90f3a873ebc6ac4d3a4
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-11-06 15:53:20 -0800

    Simple sum calculator

--- hello.py
+++ hello.py
@@ -2,11 +2,16 @@ message = "Hello World!"
 employed = False
 print(message)
 
-name = input("What is your name? ")
-print("Hello " + name)
+# name = input("What is your name? ")
+# print("Hello " + name)
 
-birthYear = input("What year were you born? ")
-birthYear = int(birthYear)
+# birthYear = input("What year were you born? ")
+# birthYear = int(birthYear)
 
-age = 2024 - birthYear
-print(name + " is " + str(age) + " years old")
+# age = 2024 - birthYear
+# print(name + " is " + str(age) + " years old")
+
+A = input("First number? ")
+B = input("Second number? ")
+C = float(A) + float(B)
+print("Sum: " + str(C))
