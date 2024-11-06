47a9ac930439c30a4afa9fd66ce2fdd7049c8d62
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-11-06 15:42:42 -0800

    Practice basic commands

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
 message = "Hello World!"
+employed = False
 print(message)
+
+name = input("What is your name? ")
+print("Hello " + name)
+
+birthYear = input("What year were you born? ")
+birthYear = int(birthYear)
+
+age = 2024 - birthYear
+print(name + " is " + str(age) + " years old")
