b78a249f6dd07758bd2f5bfb4aae36c95832e7ea
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-11-08 14:45:23 -0800

    Practice conditional statements

--- hello.py
+++ hello.py
@@ -15,5 +15,28 @@ print(message)
 # B = input("Second number? ")
 # C = float(A) + float(B)
 # print("Sum: " + str(C))
+A = 3 > 2
+print("A: " + str(A))
 
-    # Python python python
+price = 25
+
+print(price > 10 and price < 30)
+# ^^ above will return true only if expressions on both sides of "and" are true
+salePrice = 15
+print(salePrice > 10 or salePrice < 3)
+# ^^ above will return true if either expression is true
+
+
+temperature = 25
+if temperature > 30:
+    # This indented code will only run if the condition above is met
+    print("It's hot outside! ")
+elif temperature > 20:
+    # elif is short for else if
+    print("It's a nice day")
+elif temperature > 10:
+    print("it's a bit cold") 
+else:
+    print("It's friggen cold out there! ")
+print("done")
+# ^ this always runs
