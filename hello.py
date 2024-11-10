2021676a52d094c7668de80c9e5a3851effd09e0
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-11-09 17:00:01 -0800

    Complete range lesson

--- hello.py
+++ hello.py
@@ -56,18 +56,30 @@ print(salePrice > 10 or salePrice < 3)
 #     print("Something went wrong")
 
 
-i = 1
-while i < 10:
-    print(i * "*")
-    i = i + 1
+# i = 1
+# while i < 10:
+#     print(i * "*")
+#     i = i + 1
+# # ^^ This demonstrates a special function that allows us to multiply i by a string and print the string i times
 
 
-numbers = range(5)
-print(numbers)
-# The range function should return the numbers from zero to the number specified, excluding the number specified. 
-# So basically range(5) = [0, 1, 2, 3, 4]
-# BUT it won't log that way if we print it. It will just log range(0, 5)
-# To get each number, we can use a for loop: 
-for number in numbers: 
+# numbers = range(5)
+# print(numbers)
+# # The range function should return the numbers from zero to the number specified, excluding the number specified. 
+# # So basically range(5) = [0, 1, 2, 3, 4]
+# # BUT it won't log that way if we print it. It will just log range(0, 5)
+# # To get each number, we can use a for loop: 
+# for number in numbers: 
+#     print(number)
+# # This^^ will print 0, 1, 2, 3, and 4  (each on thieri own line, as we're calling a print statement for each)
+
+# numbers = range(5, 10)
+# for number in numbers:
+#     print(number)
+# # If we include two arguments in our range function it will be the range from first to the second, excluding the second number
+
+numbers = range(5, 10, 2)
+for number in numbers:
     print(number)
-# This^^ will print 0, 1, 2, 3, and 4  (each on thieri own line, as we're calling a print statement for each)
+# If we include a third argument, for example two, it will return each nth value. So if two, this would return 5, 7, 9
+
